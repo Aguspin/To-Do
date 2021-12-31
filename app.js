7f1555b1e64a95e578aa8ee078d7be05b8185850
Agu_spin <agu1spin@gmail.com>
2021-12-31 11:55:50 -0600

    Se crea el funcionamiento de los botones de mas, menos y reiniciar

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const input = document.getElementById('input')
 const listaTarea =document.getElementById('lista-tareas')
 const template = document.getElementById('template').content
 const fragment = document.createDocumentFragment()
-let tareas = {
-}
+
+let tareas = {}
+
+document.addEventListener('DOMContentLoaded', () =>{
+    pintarTareas()
+})
+
+listaTarea.addEventListener('click', e => {
+    btnAccion(e)
+})
 
 //console.log(Date.now())
 
@@ -37,12 +45,56 @@ const setTarea = e =>{
 }
 
 const pintarTareas = () =>{
-        listaTarea.innerHTML = ''
-        Object.values(tareas).forEach(tarea =>{
-            const clone = template.cloneNode(true)
-            clone.querySelector('p').textContent=tarea.texto
-            fragment.appendChild(clone)
-        })
-        listaTarea.appendChild(fragment) 
 
+    if(Object.values(tareas).length === 0){
+        listaTarea.innerHTML = `
+        <div class = "alert alert-dark text-center">
+            No hay tareas :)
+        </div>
+        `
+        return
+    }
+
+    listaTarea.innerHTML = ''
+    Object.values(tareas).forEach(tarea =>{
+        const clone = template.cloneNode(true)
+        clone.querySelector('p').textContent=tarea.texto
+
+        if(tarea.estado){
+            clone.querySelector('.alert').classList.replace('alert-warning', 'alert-primary')
+            clone.querySelectorAll('.fas')[0].classList.replace('fa-check-circle','fa-undo-alt')
+            clone.querySelector('p').style.textDecoration = 'line-through'
         }
+
+        clone.querySelectorAll('.fas')[0].dataset.id = tarea.id
+        clone.querySelectorAll('.fas')[1].dataset.id = tarea.id
+        fragment.appendChild(clone)
+    })
+    listaTarea.appendChild(fragment) 
+}
+
+
+const btnAccion = e => {
+    //console.log(e.target.classList.contains('fa-check-circle'))
+    if(e.target.classList.contains('fa-check-circle')){
+        //console.log(e.target.dataset.id)
+        tareas[e.target.dataset.id].estado = true
+        pintarTareas()
+        //console.log(tareas)
+    }
+
+    if(e.target.classList.contains('fa-minus-circle')){
+        delete tareas[e.target.dataset.id]
+        pintarTareas()
+
+    }
+
+    if(e.target.classList.contains('fa-undo-alt')){
+        tareas[e.target.dataset.id].estado = false
+        pintarTareas()
+        //console.log(tareas)
+    }
+    
+
+    e.stopPropagation()
+}
